aa2da83b287a487b94c4e0ef38cc21145aa290a3
Ayan Das Karmakar <coderayan1411@gmail.com>
2023-07-16 11:35:39 +0530

    Integrated Google Analytics

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const page = () => {
+    return (
+        <>
+            <Link href="/">Home</Link>
+            <div>About Page</div>
+        </>
+    )
+}
+
+export default page
